dc6545002a69a8d22b69e24066aeb05cb54897e4
YuNanXing <1224678297@qq.com>
2023-08-01 15:57:40 +0800

    feat:  集成sass

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,15 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),createSvgIconsPlugin({
-    // Specify the icon folder to be cached
-    iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
-    // Specify symbolId format
-    symbolId: 'icon-[dir]-[name]',
-  }),],
+  plugins: [
+    vue(),
+    createSvgIconsPlugin({
+      // Specify the icon folder to be cached
+      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+      // Specify symbolId format
+      symbolId: 'icon-[dir]-[name]',
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve('./src'), // 相对路径别名配置，使用 @ 代替 src
