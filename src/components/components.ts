22f99f13fd078da0ea21c126e608af4be07335db
YuNanXing <1224678297@qq.com>
2023-08-01 15:57:18 +0800

    feat:  集成sass

---
+++ src/components/components.ts
@@ -0,0 +1,8 @@
+/*
+ * @Description: Stay hungry，Stay foolish
+ * @Author: Huccct
+ * @Date: 2023-05-17 20:27:16
+ * @LastEditors: Huccct
+ * @LastEditTime: 2023-05-17 20:47:21
+ */
+export { DefSvgIcon } from './SvgIcon'
--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,15 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 // @ts-expect-error忽略当前文件ts类型的检测否则有红色提示(打包会失败)
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
-
+import 'virtual:svg-icons-register'
+import globalComponent from './components/index'
+//引入css样式
+import '@/styles/index.scss'
 
 const app = createApp(App)
 app.use(ElementPlus, {
-  locale: zhCn
+  locale: zhCn,
 })
+app.use(globalComponent)
+
 app.mount('#app')
