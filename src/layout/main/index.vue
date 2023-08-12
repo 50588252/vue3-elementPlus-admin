29c5596108055825fb3825598a57311c884d65b9
YuNanXing <1224678297@qq.com>
2023-08-12 22:25:54 +0800

    feat: 完善折叠

--- src/layout/main/index.vue
+++ src/layout/main/index.vue
@@ -1,26 +1,19 @@
-<template>
-  <!-- 路由组件出口的位置 -->
-  <router-view v-slot="{ Component }">
-    <transition name="fade">
-      <!-- 渲染layout一级路由组件的子路由 -->
-      <component :is="Component" v-if="flag" />
-    </transition>
-  </router-view>
-</template>
-
+<!--
+ * @Description: Stay hungry，Stay foolish
+ * @Author: Huccct
+ * @Date: 2023-05-21 17:22:48
+ * @LastEditors: Huccct
+ * @LastEditTime: 2023-05-21 20:58:27
+-->
 <script setup lang="ts">
-import { watch, ref, nextTick } from 'vue'
 import useLayOutSettingStore from '@/store/modules/setting'
+import { watch, ref, nextTick } from 'vue'
 let layOutSettingStore = useLayOutSettingStore()
 
-//控制当前组件是否销毁重建
 let flag = ref(true)
-
-//监听仓库内部数据是否发生变化,如果发生变化，说明用户点击过刷新按钮
 watch(
   () => layOutSettingStore.refsh,
   () => {
-    //点击刷新按钮:路由组件销毁
     flag.value = false
     nextTick(() => {
       flag.value = true
@@ -28,18 +21,19 @@ watch(
   },
 )
 </script>
-<script lang="ts">
-export default {
-  name: 'Main',
-}
-</script>
-
-<style scoped>
+<template>
+  <!-- 路由组件出口的位置 -->
+  <router-view v-slot="{ Component }">
+    <transition name="fade">
+      <component :is="Component" v-if="flag" />
+    </transition>
+  </router-view>
+</template>
+<style lang="scss" scoped>
 .fade-enter-from {
   opacity: 0;
   transform: scale(0);
 }
-
 .fade-enter-active {
   transition: all 0.3s;
 }
--- src/layout/tabbar/breadcrumb/index.vue
+++ src/layout/tabbar/breadcrumb/index.vue
@@ -1,45 +1,33 @@
+<script setup lang="ts">
+import { ArrowRight, Fold, Expand } from '@element-plus/icons-vue'
+import useLayOutSettingStore from '@/store/modules/setting'
+let LayOutSettingStore = useLayOutSettingStore()
+const changeIcon = () => {
+  LayOutSettingStore.isCollapse = !LayOutSettingStore.isCollapse
+}
+</script>
 <template>
-  <!-- 顶部左侧静态 -->
-  <el-icon style="margin-right: 10px" @click="changeIcon">
-    <component :is="LayOutSettingStore.fold ? 'Fold' : 'Expand'"></component>
+  <el-icon
+    style="margin-right: 10px; cursor: pointer"
+    size="20px"
+    @click="changeIcon"
+  >
+    <component :is="LayOutSettingStore.isCollapse ? Expand : Fold"></component>
   </el-icon>
-  <!-- 左侧面包屑 -->
-  <el-breadcrumb separator-icon="ArrowRight">
-    <!-- 面包动态展示路由名字与标题 -->
+  <el-breadcrumb separator="/" :separator-icon="ArrowRight">
     <el-breadcrumb-item
       v-for="(item, index) in $route.matched"
       :key="index"
       v-show="item.meta.title"
       :to="item.path"
     >
-      <!-- 图标 -->
-      <el-icon>
+      <el-icon style="vertical-align: middle">
         <component :is="item.meta.icon"></component>
       </el-icon>
-      <!-- 面包屑展示匹配路由的标题 -->
-      <span>{{ item.meta.title }}</span>
+      <span style="margin: 0 5px; vertical-align: middle">
+        {{ item.meta.title }}
+      </span>
     </el-breadcrumb-item>
   </el-breadcrumb>
 </template>
-
-<script setup lang="ts">
-import { useRoute } from 'vue-router'
-import { ref } from 'vue'
-import useLayOutSettingStore from '@/store/modules/setting'
-//获取layout配置相关的仓库
-let LayOutSettingStore = useLayOutSettingStore()
-//获取路由对象
-let $route = useRoute()
-//点击图标的方法
-const changeIcon = () => {
-  //图标进行切换
-  LayOutSettingStore.fold = !LayOutSettingStore.fold
-}
-</script>
-<script lang="ts">
-export default {
-  name: 'Breadcrumb',
-}
-</script>
-
-<style scoped></style>
+<style lang="scss" scoped></style>
