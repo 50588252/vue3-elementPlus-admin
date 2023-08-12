da1089a894108e098a31fb0a3c4bee5018d4968c
YuNanXing <1224678297@qq.com>
2023-08-12 18:42:59 +0800

    feat: 完善折叠

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,75 +1,109 @@
 <template>
-  <div class="layout_container">
-    <div class="layout_slider">
-      <Logo></Logo>
-      <el-scrollbar class="scrollbar">
-        <el-menu
-          :default-active="$route.path"
-          background-color="#001529"
-          text-color="white"
-        >
-          <Menu :menuList="userStore.menuRoutes"></Menu>
-        </el-menu>
-      </el-scrollbar>
+    <div class="layout_container">
+        <!-- 左侧菜单 -->
+        <div class="layout_slider">
+            <Logo></Logo>
+            <!-- 展示菜单 -->
+            <!-- 滚动组件 -->
+            <el-scrollbar class="scrollbar">
+                <!-- 菜单组件-->
+                <el-menu :collapse="LayOutSettingStore.fold?true:false" :default-active="$route.path" background-color="#001529" text-color="white"
+                    active-text-color="yellowgreen">
+                    <!--根据路由动态生成菜单-->
+                    <Menu :menuList="userStore.menuRoutes"></Menu>
+                </el-menu>
+            </el-scrollbar>
+        </div>
+        <!-- 顶部导航 -->
+        <div class="layout_tabbar" :class="{ fold: LayOutSettingStore.fold ? true : false }">
+            <!-- layout组件的顶部导航tabbar -->
+            <Tabbar></Tabbar>
+        </div>
+        <!-- 内容展示区域 -->
+        <div class="layout_main" :class="{ fold: LayOutSettingStore.fold ? true : false }">
+            <Main></Main>
+        </div>
     </div>
-    <div class="layout_tabbar">456</div>
-    <div class="layout_main">
-      <Main></Main>
-    </div>
-  </div>
 </template>
 
 <script setup lang="ts">
+//获取路由对象
 import { useRoute } from 'vue-router'
+//引入左侧菜单logo子组件
 import Logo from './logo/index.vue'
+//引入菜单组件
 import Menu from './menu/index.vue'
-import Main from './main/index.vue'
-
+//右侧内容展示区域
+import Main from './main/index.vue';
+//引入顶部tabbar组件
+import Tabbar from './tabbar/index.vue';
 //获取用户相关的小仓库
-import useUserStore from '@/store/modules/user'
-let userStore = useUserStore()
+import useUserStore from '@/store/modules/user';
+import useLayOutSettingStore from '@/store/modules/setting';
+let userStore = useUserStore();
+//获取layout配置仓库
 
-let $route = useRoute()
+let LayOutSettingStore = useLayOutSettingStore();
+
+//获取路由对象
+let $route = useRoute();
 </script>
 
+<script lang="ts">
+export default {
+    name: "Layout"
+}
+</script>
 <style scoped lang="scss">
 .layout_container {
-  width: 100%;
-  height: 100vh;
-  background-color: red;
-
-  .layout_slider {
-    width: $base-menu-width;
+    width: 100%;
     height: 100vh;
-    background-color: $base-menu-background;
-    color: white;
-    .scrollbar {
-      width: 100%;
-      height: calc(100vh - $base-menu-logo-height);
-      .el-menu {
-        border-right: none;
-      }
+
+    .layout_slider {
+        color: white;
+        width: $base-menu-width;
+        height: 100vh;
+        background: $base-menu-background;
+        transition: all 0.3s;
+
+        .scrollbar {
+            width: 100%;
+            height: calc(100vh - $base-menu-logo-height);
+
+            .el-menu {
+                border-right: none;
+            }
+        }
     }
-  }
 
-  .layout_tabbar {
-    position: fixed;
-    top: 0;
-    left: $base-menu-width;
-    width: calc(100% - $base-menu-width);
-    height: $base-tabbar-height;
-    background-color: green;
-  }
+    .layout_tabbar {
+        position: fixed;
+        width: calc(100% - $base-menu-width);
+        height: $base-tabbar-height;
+        top: 0px;
+        left: $base-menu-width;
+        transition: all 0.3s;
+        &.fold {
+            width: calc(100vw - $base-menu-min-width );
+            left: $base-menu-min-width;
+        }
+    }
 
-  .layout_main {
-    position: absolute;
-    top: $base-tabbar-height;
-    left: $base-menu-width;
-    width: calc(100% - $base-menu-width);
-    height: calc(100vh - $base-tabbar-height);
-    padding: 20px;
-    overflow: auto;
-    transition: all 0.3s;
-  }
+    .layout_main {
+        position: absolute;
+        width: calc(100% - $base-menu-width);
+        height: calc(100vh - $base-tabbar-height);
+        left: $base-menu-width;
+        top: $base-tabbar-height;
+        padding: 20px;
+        overflow: auto;
+        transition: all 0.3s;
+
+        &.fold {
+            width: calc(100vw - $base-menu-min-width );
+            left: $base-menu-min-width;
+        }
+
+    }
 }
 </style>
